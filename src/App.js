ad7285821cc341b6301b858555f4b2b5181f387e
ZainSaleem1110 <szain0120@gmail.com>
2022-05-20 13:40:45 +0500

    integrate tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className="bg-gray-700 text-white py-4 text-center font-bold">
+            Music Player
+        </div>
+    )
+}
+
+export default App
