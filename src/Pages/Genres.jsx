4998898b74f8202fa8bce817fac22c935b3fb0e9
ZainSaleem1110 <szain0120@gmail.com>
2022-05-24 10:17:24 +0500

    genres

--- src/Pages/Genres.jsx
+++ src/Pages/Genres.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Img from '../assets/others.jpg'
 
 function Genres() {
     return (
-        <div className="text-[white]">
-            Genres
+        <div className="text-[white] w-[200px] h-[200px] rounded-lg othersAlbum relative cursor-pointer">
+            <img src={Img} className="rounded-lg" alt="" />
+            <div className="absolute top-0 left-0 othersAlbum w-[200px] h-[200px] rounded-lg flex justify-center items-center">
+                <h1 className="text-[28px] font-bold">Other</h1>
+            </div>
         </div>
     )
 }
