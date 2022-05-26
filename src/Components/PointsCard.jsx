6a5b0433f94c511899da59d4ad91a4538c37fff8
ZainSaleem1110 <szain0120@gmail.com>
2022-05-27 03:19:06 +0500

    points page

--- src/Components/PointsCard.jsx
+++ src/Components/PointsCard.jsx
@@ -8,9 +8,9 @@ function PointsCard({Data}) {
         <div className="mt-5 flex flex-wrap gap-6">
             {dataList && dataList.map((list,index)=>{
                 return <div className="w-[250px] h-[200px] rounded-lg bg-[#222225] flex flex-col justify-center items-center">
-                <div className="w-[50px] h-[50px] bg-[#3a3116] text-[#FFC107] rounded-full flex justify-center items-center text-[24px] relative">
+                <div className="w-[50px] h-[50px] bg-[#4e391e] text-[#f98f1d] rounded-full flex justify-center items-center text-[20px] relative">
                     {list.Icon}
-                    <div className="absolute bottom-[-8px] right-[-8px] rounded-full w-[28px] h-[28px] bg-[#F98F1D] flex justify-center items-center text-white text-[13px]">
+                    <div className="absolute bottom-[-8px] right-[-8px] rounded-full w-[28px] h-[28px] bg-[#f98f1d] flex justify-center items-center text-white text-[13px]">
                         {list.points}
                     </div>
                 </div>
