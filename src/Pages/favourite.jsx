737505ed727fcc25e01d83b7bed515016dadd79e
ZainSaleem1110 <szain0120@gmail.com>
2022-05-30 14:33:55 +0500

    fav page

--- src/Pages/favourite.jsx
+++ src/Pages/favourite.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {AiOutlineStar} from 'react-icons/ai'
 
 function favourite() {
     return (
         <div className="text-[white]">
-            Favourite
+            <div className="flex items-center text-white border-b border-[#ffffff4d] w-[100%] pb-3 mb-4 mt-10">
+                <div className="w-[32px] h-[32px] bg-[#e4ab00] flex justify-center items-center rounded-full">
+                    <AiOutlineStar className="text-[16px]" />
+                </div>
+                <h1 className="text-[22px] ml-3">My Favourites</h1>
+            </div>
+            <p className="mt-3">You currently have 0 favourite songs</p>
         </div>
     )
 }
