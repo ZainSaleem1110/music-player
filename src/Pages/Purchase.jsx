db26687ba9d2187cb6849b405e5116515ec70045
ZainSaleem1110 <szain0120@gmail.com>
2022-05-30 13:08:15 +0500

    recent playlist

--- src/Pages/Purchase.jsx
+++ src/Pages/Purchase.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {IoMdAppstore} from 'react-icons/io'
 
 function Purchase() {
     return (
         <div className="text-[white]">
-            Purchase
+            <div className="flex items-center text-white border-b border-[#ffffff4d] w-[100%] pb-3 mb-4 mt-10">
+                <div className="w-[32px] h-[32px] bg-[#3e50b3] flex justify-center items-center rounded-full">
+                    <IoMdAppstore className="text-[16px]" />
+                </div>
+                <h1 className="text-[22px] ml-3">My Purchases</h1>
+            </div>
+            <p className="mt-5 text-center">No purchased tracks found.</p>
         </div>
     )
 }
--- src/Pages/RecentPlayed.jsx
+++ src/Pages/RecentPlayed.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { MdReplay10 } from 'react-icons/md'
+import SongCard from '../Components/songCard'
+import Song01 from '../assets/songs1.jpeg'
+
+const NewPlayed = [
+    { Image: Song01, name: "M.G'$ starring M...", country: "Newyauk" },
+]
 
 function RecentPlayed() {
     return (
-        <div className="text-[white]">
-            Recent Played
-        </div>
+        <>
+            <div className="mt-12 flex justify-between items-center">
+                <div className="flex items-center text-white">
+                <div className="w-[32px] h-[32px] bg-[#4caf50] flex justify-center items-center rounded-full">
+                        <MdReplay10 className="text-[24px]" />
+                    </div>
+                    <h1 className="text-[22px] ml-3">Recently Played</h1>
+                </div>
+            </div>
+            <div className="h-[1px] bg-[#ffffff4d] mt-4"></div>
+            <SongCard Data={NewPlayed} />
+            <div className="flex justify-center mt-10 ">
+                    <button className="text-[#f98f1d] bg-[#2C2C2F] px-14 py-1 rounded-full ">
+                        Load More
+                    </button>
+                </div>
+        </>
     )
 }
 
