bb667eb1e5912af921c17eebcc5871400cb50af0
ZainSaleem1110 <szain0120@gmail.com>
2022-06-01 09:18:49 +0500

    sidebar active option

--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Logo from '../assets/logo.jpeg'
 import { Link } from 'react-router-dom'
 import { BsMusicNoteBeamed, BsMusicNote, BsGraphUp, BsFillCalendarEventFill } from 'react-icons/bs'
-import { MdLibraryMusic,MdLoyalty, MdPlaylistPlay, MdReplay10 } from 'react-icons/md'
+import { MdLibraryMusic, MdLoyalty, MdPlaylistPlay, MdReplay10 } from 'react-icons/md'
 import { RiDropFill, RiExchangeDollarLine, RiPlayListFill } from 'react-icons/ri'
 import { FiBarChart, FiBox } from 'react-icons/fi'
 import { FaStoreAlt } from 'react-icons/fa'
@@ -11,84 +11,95 @@ import { AiOutlineStar } from 'react-icons/ai'
 
 function Sidebar() {
 
+    const SelectOption=()=>{
+        var sidebarParent=document.querySelector('.sidebarParent')
+        var sidebarOption=document.querySelectorAll('.sidebarOption')
+        sidebarOption.forEach(el=>{
+            el.addEventListener('click',()=>{
+                sidebarParent.querySelector('.active').classList.remove('active')
+                el.classList.add('active')
+            })
+        })
+    }
+
     return (
         <>
             <div className="lg:w-[18%] w-[70px] h-[100vh] bg-[#222225] overflow-y-auto md:flex hidden overflow-x-hidden">
                 <div className="flex bg-[black] items-center justify-center fixed top-0 left-0 w-[18%] bg-[#222225] lg:flex hidden h-[60px]">
                     <img src={Logo} className="w-[50px] h-[47px]" alt="" />
                 </div>
-                <div className="text-white lg:my-24 my-4 font-bold">
+                <div className="text-white lg:my-24 my-4 font-bold sidebarParent">
                     <h1 className="text-[12px] pl-6 text-[lightgray] lg:flex hidden">BROWSER MUSIC</h1>
-                    <Link to="/" className="h-[40px] flex items-center text-[15px] pl-6  mt-3 btn active">
+                    <Link to="/" className="h-[40px] flex items-center text-[15px] pl-6  mt-3 btn active sidebarOption" onClick={SelectOption}>
                         <BsMusicNoteBeamed className="text-[20px]" />
                         <span className="ml-7">Discover</span>
                     </Link>
-                    <Link to="/newMusic" className="h-[40px] flex items-center text-[15px] pl-6 btn ">
+                    <Link to="/newMusic" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <BsMusicNote className="text-[20px]" />
                         <span className="ml-7">New Music</span>
                     </Link>
-                    <Link to="/topMusic" className="h-[40px] flex items-center text-[15px] pl-6 btn ">
+                    <Link to="/topMusic" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <BsGraphUp className="text-[20px]" />
                         <span className="ml-7">Top Music</span>
                     </Link>
-                    <Link to="/albums" className="h-[40px] flex items-center text-[15px] pl-6 btn">
+                    <Link to="/albums" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <MdLibraryMusic className="text-[20px]" />
                         <span className="ml-7">Albums</span>
                     </Link>
-                    <Link to="/sportLight" className="h-[40px] flex items-center text-[15px] pl-6 btn">
+                    <Link to="/sportLight" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <RiDropFill className="text-[20px]" />
                         <span className="ml-7">Sportlight</span>
                     </Link>
-                    <Link to="/genres" className="h-[40px] flex items-center text-[15px] pl-6 btn">
+                    <Link to="/genres" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <FiBox className="text-[20px]" />
                         <span className="ml-7">Genres</span>
                     </Link>
-                    <Link to="/playlists" className="h-[40px] flex items-center text-[15px] pl-6 btn">
+                    <Link to="/playlists" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <MdPlaylistPlay className="text-[28px]" />
                         <span className="ml-5">Playlists</span>
                     </Link>
-                    <Link to="/fame" className="h-[40px] flex items-center text-[15px] pl-6 btn">
+                    <Link to="/fame" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <FiBarChart className="text-[20px]" />
                         <span className="ml-7">Hall of fame</span>
                     </Link>
-                    <Link to="/points" className="h-[40px] flex items-center text-[15px] pl-6 btn">
+                    <Link to="/points" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <RiExchangeDollarLine className="text-[20px]" />
                         <span className="ml-7">Earn Points</span>
                     </Link>
                     <h1 className="text-[12px] pl-6  mt-7 text-[lightgray] lg:h-auto md:h-[1px] lg:bg-[transparent] bg-[lightgray]">
                         <span className="lg:flex hidden">STORE</span>
                     </h1>
-                    <Link to="/browse" className="h-[40px] flex items-center text-[15px] pl-6  mt-3 btn">
+                    <Link to="/browse" className="h-[40px] flex items-center text-[15px] pl-6  mt-3 btn sidebarOption" onClick={SelectOption}>
                         <FaStoreAlt className="text-[20px]" />
                         <span className="ml-7">Browse</span>
                     </Link>
-                    <Link to="/purchase" className="h-[40px] flex items-center text-[15px] pl-6 btn">
+                    <Link to="/purchase" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <IoMdAppstore className="text-[20px]" />
                         <span className="ml-7">Purchased</span>
                     </Link>
                     <h1 className="text-[12px] pl-6  mt-7 text-[lightgray] lg:h-auto md:h-[1px] lg:bg-[transparent] bg-[lightgray]">
                         <span className="lg:flex hidden">YOUR MUSIC</span>
                     </h1>
-                    <Link to="/recentPlayed" className="h-[40px] flex items-center text-[15px] pl-6 mt-3 btn">
+                    <Link to="/recentPlayed" className="h-[40px] flex items-center text-[15px] pl-6 mt-3 btn sidebarOption" onClick={SelectOption}>
                         <MdReplay10 className="text-[28px]" />
                         <span className="lg:ml-5 ml-7">Recently Played</span>
                     </Link>
-                    <Link to="/myPlaylist" className="h-[40px] flex items-center text-[15px] pl-6 btn">
+                    <Link to="/myPlaylist" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <RiPlayListFill className="text-[20px]" />
                         <span className="ml-7">My Playlists</span>
                     </Link>
-                    <Link to="/favourite" className="h-[40px] flex items-center text-[15px] pl-6 btn">
+                    <Link to="/favourite" className="h-[40px] flex items-center text-[15px] pl-6 btn sidebarOption" onClick={SelectOption}>
                         <AiOutlineStar className="text-[20px]" />
                         <span className="ml-7">Favourite</span>
                     </Link>
                     <h1 className="text-[12px] pl-6  mt-7 text-[lightgray] lg:h-auto md:h-[1px] lg:bg-[transparent] bg-[lightgray]">
                         <span className="lg:flex hidden">Events</span>
                     </h1>
-                    <Link to="/event" className="h-[40px] flex items-center text-[15px] pl-6  lg:mt-3 mt-7 btn">
+                    <Link to="/event" className="h-[40px] flex items-center text-[15px] pl-6  lg:mt-3 mt-7 btn sidebarOption" onClick={SelectOption}>
                         <BsFillCalendarEventFill className="text-[20px]" />
                         <span className="ml-7">Events</span>
                     </Link>
-                    <Link to="/loyality" className="h-[40px] flex items-center text-[15px] pl-6  lg:mt-3 mt-7 btn">
+                    <Link to="/loyality" className="h-[40px] flex items-center text-[15px] pl-6  lg:mt-3 mt-7 btn sidebarOption" onClick={SelectOption}>
                         <MdLoyalty className="text-[20px]" />
                         <span className="ml-7">Loyalty Split</span>
                     </Link>
